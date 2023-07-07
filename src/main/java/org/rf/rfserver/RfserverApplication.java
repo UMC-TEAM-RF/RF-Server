c66a5fef88a350f744b80034d89dc35a5e2cf31a
jaewonLeeKOR <ghfkddl706@me.com>
2023-07-07 13:14:24 +0900

    project started

---
+++ src/main/java/org/rf/rfserver/RfserverApplication.java
@@ -0,0 +1,13 @@
+package org.rf.rfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfserverApplication.class, args);
+	}
+
+}
