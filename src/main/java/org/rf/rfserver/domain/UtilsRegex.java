746b0b3a053060d172f025ba8b6ba1d898730ab4
minjgziii <kmj4668@gmail.com>
2023-07-17 23:57:21 +0900

    feature: [#9] 학교 이메일 확인을 위한 정규식 작성 및 학교 이름 추출을 위한 상수 선언

---
+++ src/main/java/org/rf/rfserver/domain/UtilsRegex.java
@@ -0,0 +1,14 @@
+package org.rf.rfserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UtilsRegex {
+    UNIVERSITY_MAIL("UNIVERSITY_MAIL_REGEX", "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.+a+c+\\.+k+r+$"),  // 학교 메일 확인 정규식
+    MAIL_PARSE("MAIL_PARSE_REGEX", "[@.]");     // "@"와 "."를 구분자로 사용해 학교 이름 추출
+
+    private final String name;
+    private final String regex;
+}
