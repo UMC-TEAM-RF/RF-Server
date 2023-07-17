a84e6888f2f284a94bf73e8b7abf7cad28b169b0
minjgziii <kmj4668@gmail.com>
2023-07-18 00:00:07 +0900

    feature: [#9] 학교 인증을 위해 인증코드를 담은 이메일을 발송할 때 보낼 message 작성

---
+++ src/main/java/org/rf/rfserver/domain/UtilsMessage.java
@@ -0,0 +1,16 @@
+package org.rf.rfserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UtilsMessage {
+    AUTH_TITLE("[알프] 인증번호를 발송했습니다."),
+    AUTH_MESSAGE("[학교 인증을 위한 메일]\n"
+            + "이메일 인증을 진행해 주세요.\n"
+            + "아래의 인증번호를 입력하여 회원가입을 완료하세요.\n")
+    ;
+
+    private final String content;
+}
