49e209c7b70e5e384012f8ccc5b78b5ded5f634f
minjgziii <kmj4668@gmail.com>
2023-07-18 00:00:34 +0900

    feature: [#9] Utils dto 작성 완료

---
+++ src/main/java/org/rf/rfserver/utils/dto/UtilsRequest.java
@@ -0,0 +1,13 @@
+package org.rf.rfserver.utils.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UtilsRequest {
+    private String mail;
+    private String code;
+}
---
+++ src/main/java/org/rf/rfserver/utils/dto/UtilsResponse.java
@@ -0,0 +1,11 @@
+package org.rf.rfserver.utils.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.rf.rfserver.domain.Utils;
+
+@Getter
+@AllArgsConstructor
+public class UtilsResponse {
+    private Utils utils;
+}
