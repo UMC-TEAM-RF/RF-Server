0b31728507c99f33e6f65ad099dc2bb78049f240
minjgziii <kmj4668@gmail.com>
2023-07-24 01:23:18 +0900

    feature: [#9] 학교 이메일 정규식 수정

--- src/main/java/org/rf/rfserver/domain/MailRegex.java
+++ src/main/java/org/rf/rfserver/domain/MailRegex.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MailRegex {
-    UNIVERSITY_MAIL("UNIVERSITY_MAIL_REGEX", "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.+a+c+\\.+k+r+$"),  // 학교 메일 확인 정규식
+    UNIVERSITY_MAIL("UNIVERSITY_MAIL_REGEX", "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.(ac\\.kr|edu)$"),  // 학교 메일 확인 정규식
     MAIL_PARSE("MAIL_PARSE_REGEX", "[@.]");     // "@"와 "."를 구분자로 사용해 학교 이름 추출
 
     private final String name;
