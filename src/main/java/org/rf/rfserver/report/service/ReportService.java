bdd1fb09f5eb20af2e0a98a17f1bb1d8082bbbcf
jaewonLeeKOR <ghfkddl706@me.com>
2023-08-06 21:45:03 +0900

    feat(#26) : 사용자 신고하기 API 구현 수정

--- src/main/java/org/rf/rfserver/report/service/ReportService.java
+++ src/main/java/org/rf/rfserver/report/service/ReportService.java
@@ -2,7 +2,6 @@ package org.rf.rfserver.report.service;
 
 import lombok.RequiredArgsConstructor;
 import org.rf.rfserver.config.BaseException;
-import org.rf.rfserver.config.BaseResponse;
 import org.rf.rfserver.domain.Report;
 import org.rf.rfserver.domain.User;
 import org.rf.rfserver.report.dto.PostReportReq;
@@ -20,15 +19,11 @@ public class ReportService {
     private final UserRepository userRepository;
 
     public PostReportRes createUserReport(PostReportReq postReportReq) throws BaseException {
-        try {
-            User reporter = userRepository.findById(postReportReq.getReporterId())
-                    .orElseThrow(() -> new BaseException(NO_SUCH_USER));
-            User repotedUser = userRepository.findById(postReportReq.getRepotedUserId())
-                    .orElseThrow(() -> new BaseException(NO_SUCH_USER));
-            Report report = reportRepository.save(new Report(reporter, repotedUser, postReportReq.getContent()));
-            return new PostReportRes(report.getId(), report.getReportedUser().getNickName());
-        } catch(Exception e) {
-            throw new BaseException(DATABASE_ERROR);
-        }
+        User reporter = userRepository.findById(postReportReq.getReporterId())
+                .orElseThrow(() -> new BaseException(NO_SUCH_USER));
+        User repotedUser = userRepository.findById(postReportReq.getRepotedUserId())
+                .orElseThrow(() -> new BaseException(NO_SUCH_USER));
+        Report report = reportRepository.save(new Report(reporter, repotedUser, postReportReq.getContent()));
+        return new PostReportRes(report.getId(), report.getReportedUser().getNickName());
     }
 }
