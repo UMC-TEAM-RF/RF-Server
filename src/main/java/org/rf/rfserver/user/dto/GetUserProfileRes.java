e7e76f3781ee465595f3ccca404b1b0af6d0011a
kuk6933 <kuk6933@naver.com>
2023-08-06 18:25:45 +0900

    feature: [#29]: add country

--- src/main/java/org/rf/rfserver/user/dto/GetUserProfileRes.java
+++ src/main/java/org/rf/rfserver/user/dto/GetUserProfileRes.java
@@ -2,10 +2,12 @@ package org.rf.rfserver.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.rf.rfserver.constant.Country;
 
 @AllArgsConstructor
 @Getter
 public class GetUserProfileRes {
     private String nickName;
     private String imageFilePath;
+    private Country country;
 }
