c56f23263936822dc9134d6151bfc51806a073a6
jaewonLeeKOR <ghfkddl706@me.com>
2023-08-08 16:35:09 +0900

    feat(#19) : 메소드 주석 추가 및 검증 예외처리 추가

--- src/main/java/org/rf/rfserver/chat/controller/ChatController.java
+++ src/main/java/org/rf/rfserver/chat/controller/ChatController.java
@@ -18,6 +18,12 @@ import java.util.List;
 @RequestMapping("/chat")
 public class ChatController {
     private final ChatService chatService;
+
+    /**
+     * getChatsReq의 chatId 이후의 userId 값을 가진 사용자가 속한 그룹의 메시지들을 ChatDto 형식으로 조회
+     * @param getChatsReq
+     * @return List[ChatDto]
+     */
     @PostMapping()
     public BaseResponse<List<ChatDto>> getChats(@RequestBody GetChatsReq getChatsReq) {
         try {
--- src/main/java/org/rf/rfserver/chat/service/ChatService.java
+++ src/main/java/org/rf/rfserver/chat/service/ChatService.java
@@ -15,6 +15,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.rf.rfserver.config.BaseResponseStatus.NO_SUCH_USER;
+
 @Service
 @AllArgsConstructor
 public class ChatService {
@@ -23,12 +25,16 @@ public class ChatService {
 
     public List<ChatDto> getChats(GetChatsReq getChatsReq) throws BaseException {
         User user = userRepository.findById(getChatsReq.getUserId())
-                .orElseThrow();
+                .orElseThrow(() -> new BaseException(NO_SUCH_USER));
         List<Chat> chats = chatRepository.findChatsByIdAfterAndUserPartiesContainUser(getChatsReq.getChatId(), user);
         return chats.stream()
                 .map(chat -> new ChatDto(chat))
                 .collect(Collectors.toList());
     }
+
+    /**
+     * 3일이 지난 채팅들을 데이터베이스에서 제거하는 서비스 메소드
+     */
     @Scheduled(cron = "0 0 0/6 * * *")
     public void expireChats() {
         chatRepository.deleteChatsByCreatedAtBefore(LocalDateTime.now().minusDays(3));
