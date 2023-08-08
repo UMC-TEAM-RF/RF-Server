66ea98cb6cdb3217af17af928041d2b13fa7ab44
jaewonLeeKOR <ghfkddl706@me.com>
2023-08-08 15:35:17 +0900

    feat(#19) : 사용자 프로필 이미지 필드 추가

--- src/main/java/org/rf/rfserver/domain/User.java
+++ src/main/java/org/rf/rfserver/domain/User.java
@@ -32,6 +32,7 @@ public class User extends BaseEntity{
     private int love;
     private int hate;
     private String email;
+    private String imageUrl;
     private Boolean isEmailVerified;
 
     @Enumerated(EnumType.STRING)
