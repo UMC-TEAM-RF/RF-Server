ed2f03ff7853de9fe806b2dd00e26d2b3cdbec88
jaewonLeeKOR <ghfkddl706@me.com>
2023-08-08 15:36:11 +0900

    feat(#19) : 채팅의 일정 보내기 기능 사용을 위한 엔티티 수정, 레포지토리 추가

--- src/main/java/org/rf/rfserver/domain/Schedule.java
+++ src/main/java/org/rf/rfserver/domain/Schedule.java
@@ -2,9 +2,9 @@ package org.rf.rfserver.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
@@ -16,11 +16,22 @@ import static jakarta.persistence.FetchType.*;
 public class Schedule extends BaseEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDateTime date;
-    private String location;
-    private int participantCount;
     private String scheduleName;
+    private LocalDateTime datetime;
+    private String location;
+    private Long participantCount;
+    private Long alert;
 
     @ManyToOne(fetch = LAZY)
     private Party party;
+
+    @Builder
+    public Schedule(String scheduleName, LocalDateTime datetime, String location, Long participantCount, Long alert, Party party) {
+        this.scheduleName = scheduleName;
+        this.datetime = datetime;
+        this.location = location;
+        this.participantCount = participantCount;
+        this.alert = alert;
+        this.party = party;
+    }
 }
