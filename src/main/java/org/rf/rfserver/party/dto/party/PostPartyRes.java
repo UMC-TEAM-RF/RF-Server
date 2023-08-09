ff2b5039da755ecc2f634872af3b7790b589b50e
kuk6933 <kuk6933@naver.com>
2023-08-09 17:23:21 +0900

    feat: [#29] edit PostPartyRes

--- src/main/java/org/rf/rfserver/party/dto/party/PostPartyRes.java
+++ src/main/java/org/rf/rfserver/party/dto/party/PostPartyRes.java
@@ -2,13 +2,39 @@ package org.rf.rfserver.party.dto.party;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.rf.rfserver.constant.Interest;
+import org.rf.rfserver.constant.Language;
+import org.rf.rfserver.constant.PreferAges;
+import org.rf.rfserver.constant.Rule;
 
+import java.util.List;
+
+@Builder
 @Getter
 public class PostPartyRes {
-    private Long id;
+    private String name;
+    private String content;
+    private String location;
+    private Language language;
+    private String imageFilePath;
+    private PreferAges preferAges;
+    private int memberCount;
+    private int nativeCount;
+    private Long ownerId;
+    private List<Rule> rules;
+    private List<Interest> interests;
 
-    @Builder
-    public PostPartyRes(Long id) {
-        this.id = id;
+    public PostPartyRes(PostPartyReq postPartyReq) {
+        this.name = postPartyReq.getName();
+        this.content = postPartyReq.getContent();
+        this.location = postPartyReq.getLocation();
+        this.language = postPartyReq.getLanguage();
+        this.imageFilePath = postPartyReq.getImageFilePath();
+        this.preferAges = postPartyReq.getPreferAges();
+        this.memberCount = postPartyReq.getMemberCount();
+        this.nativeCount = postPartyReq.getNativeCount();
+        this.ownerId = postPartyReq.getOwnerId();
+        this.rules = postPartyReq.getRules();
+        this.interests = postPartyReq.getInterests();
     }
 }
