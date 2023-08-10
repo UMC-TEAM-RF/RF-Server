60ad1ac00b7152a396c1ea0fe39bf3334cbbf11e
minjgziii <kmj4668@gmail.com>
2023-08-10 14:22:12 +0900

    feature: [#60] mail 전송과 관련된 응답 형태 변경 및 code 삭제

--- src/main/java/org/rf/rfserver/mail/dto/PostSendRes.java
+++ src/main/java/org/rf/rfserver/mail/dto/PostSendRes.java
@@ -2,10 +2,12 @@ package org.rf.rfserver.mail.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.rf.rfserver.constant.University;
 import org.rf.rfserver.domain.Mail;
 
 @Getter
 @AllArgsConstructor
 public class PostSendRes {
-    private Mail mail;
+    private String mailAddress;
+    private University university;
 }
