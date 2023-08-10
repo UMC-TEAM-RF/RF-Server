f85dda0ebd6ccb60db6d1b0385579b32a06f359d
minjgziii <kmj4668@gmail.com>
2023-08-10 14:38:52 +0900

    feature: [#35] @Repository 어노테이션 삭제

--- src/main/java/org/rf/rfserver/blockParty/repository/BlockPartyRepository.java
+++ src/main/java/org/rf/rfserver/blockParty/repository/BlockPartyRepository.java
@@ -2,8 +2,6 @@ package org.rf.rfserver.blockParty.repository;
 
 import org.rf.rfserver.domain.BlockParty;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface BlockPartyRepository extends JpaRepository<BlockParty, Long> {
 }
