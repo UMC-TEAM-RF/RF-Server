f19700b5c20eb3b6a27b469bf97c5f44f5a71132
jaewonLeeKOR <ghfkddl706@me.com>
2023-08-12 22:25:54 +0900

    feat(#19) : Lettuce Connection Factory 로 전환

--- src/main/java/org/rf/rfserver/config/redis/RedisConfig.java
+++ src/main/java/org/rf/rfserver/config/redis/RedisConfig.java
@@ -5,6 +5,7 @@ import org.rf.rfserver.chat.service.ChatSubscriber;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -15,13 +16,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        return lettuceConnectionFactory;
+    }
+    @Bean
     public RedisMessageListenerContainer redisMessageListenerContainer(
-            RedisConnectionFactory connectionFactory
-            , MessageListenerAdapter listenerAdapter
+            MessageListenerAdapter listenerAdapter
             , ChannelTopic channelTopic
     ) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
+        container.setConnectionFactory(redisConnectionFactory());
         container.addMessageListener(listenerAdapter, channelTopic);
         return container;
     }
@@ -30,9 +35,9 @@ public class RedisConfig {
         return new MessageListenerAdapter(subscriber, "onMessage");
     }
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatDto.class));
         return redisTemplate;
