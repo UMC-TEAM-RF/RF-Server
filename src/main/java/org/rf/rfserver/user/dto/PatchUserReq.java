f90b239c43a4731d4c245472dacccf37b1d4db40
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-15 15:48:35 +0900

    feature: [#17] request dto에  기본생성자 추가

--- src/main/java/org/rf/rfserver/user/dto/PatchUserReq.java
+++ src/main/java/org/rf/rfserver/user/dto/PatchUserReq.java
@@ -2,6 +2,7 @@ package org.rf.rfserver.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.rf.rfserver.constant.Language;
 import org.rf.rfserver.constant.LifeStyle;
 import org.rf.rfserver.constant.Mbti;
@@ -9,6 +10,7 @@ import org.rf.rfserver.constant.Mbti;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class PatchUserReq {
     private String nickName = null;
