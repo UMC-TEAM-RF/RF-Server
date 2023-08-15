d207382c8a1c9b83724660bfe1b979a0a116a227
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-15 15:45:16 +0900

    feature: [#17] TokenApiController

--- src/main/java/org/rf/rfserver/sign/controller/TokenApiController.java
+++ src/main/java/org/rf/rfserver/sign/controller/TokenApiController.java
@@ -1,2 +1,26 @@
-package org.rf.rfserver.sign.controller;public class TokenApiController {
+package org.rf.rfserver.sign.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.rf.rfserver.config.BaseException;
+import org.rf.rfserver.sign.dto.CreateAccessTokenReq;
+import org.rf.rfserver.sign.dto.CreateAccessTokenRes;
+import org.rf.rfserver.sign.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+    private final TokenService tokenService;
+
+    @PostMapping("/token")
+    public ResponseEntity<CreateAccessTokenRes> createNewAccessToken(@RequestBody CreateAccessTokenReq request) throws BaseException {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenRes(newAccessToken));
+//        return new BaseResponse<>(new CreateAccessTokenRes(newAccessToken));
+    }
 }
