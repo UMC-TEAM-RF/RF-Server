cbedea793abadb33f225366ff8ed86b303fd8996
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-15 15:44:17 +0900

    feature: [#17]새 entity 추가 -  RefreshToken

--- src/main/java/org/rf/rfserver/domain/RefreshToken.java
+++ src/main/java/org/rf/rfserver/domain/RefreshToken.java
@@ -1,2 +1,29 @@
-package org.rf.rfserver.domain;public class ReFreshToken {
+package org.rf.rfserver.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RefreshToken {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private String refreshToken;
+
+
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
 }
