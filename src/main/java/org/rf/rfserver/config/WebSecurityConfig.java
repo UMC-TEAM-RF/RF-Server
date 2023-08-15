d852fd747163b48028814095ee7daaff949fd0fb
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-15 15:40:48 +0900

    feature: [#17] WebSecurityConfig 추가

--- src/main/java/org/rf/rfserver/config/WebSecurityConfig.java
+++ src/main/java/org/rf/rfserver/config/WebSecurityConfig.java
@@ -1,2 +1,52 @@
-package org.rf.rfserver.config;public class WebSecurityConfig {
+package org.rf.rfserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.rf.rfserver.user.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests ->
+                        requests.requestMatchers("/", "/swagger-ui/**", "/user", "/party"
+                                        , "/user/login").permitAll()	// requestMatchers의 인자로 전달된 url은 모두에게 허용
+                                .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )	// 세션을 사용하지 않으므로 STATELESS 설정
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+                                                       BCryptPasswordEncoder passwordEncoder,
+                                                       UserDetailsService userDetailsService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
