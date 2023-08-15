5c0aeee8f0f2524ac31cd697b76be4232b247a78
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-15 15:45:41 +0900

    feature: [#17] RefreshTokenService

--- src/main/java/org/rf/rfserver/sign/service/RefreshTokenService.java
+++ src/main/java/org/rf/rfserver/sign/service/RefreshTokenService.java
@@ -1,2 +1,20 @@
-package org.rf.rfserver.sign.service;public class RefreshTokenService {
+package org.rf.rfserver.sign.service;
+
+import lombok.RequiredArgsConstructor;
+import org.rf.rfserver.config.BaseException;
+import org.rf.rfserver.domain.RefreshToken;
+import org.rf.rfserver.sign.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+import static org.rf.rfserver.config.BaseResponseStatus.INVALID_TOKEN;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) throws BaseException {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new BaseException(INVALID_TOKEN));
+    }
 }
