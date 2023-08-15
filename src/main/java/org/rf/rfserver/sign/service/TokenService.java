38d592d5f0c2bdc487b15f41fe280d0d7399be46
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-15 15:46:12 +0900

    feature: [#17] TokenService

--- src/main/java/org/rf/rfserver/sign/service/TokenService.java
+++ src/main/java/org/rf/rfserver/sign/service/TokenService.java
@@ -1,2 +1,31 @@
-package org.rf.rfserver.sign.service;public class TokenService {
+package org.rf.rfserver.sign.service;
+
+import lombok.RequiredArgsConstructor;
+import org.rf.rfserver.config.BaseException;
+import org.rf.rfserver.config.jwt.TokenProvider;
+import org.rf.rfserver.constant.RfRule;
+import org.rf.rfserver.domain.User;
+import org.rf.rfserver.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+import static org.rf.rfserver.config.BaseResponseStatus.INVALID_TOKEN;
+import static org.rf.rfserver.constant.RfRule.*;
+
+@RequiredArgsConstructor
+@Service
+public class TokenService {
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken) throws BaseException {
+        if(!tokenProvider.validToken(refreshToken)) {
+            throw new BaseException(INVALID_TOKEN);
+        }
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findUserById(userId);
+        return tokenProvider.generateToken(user, Duration.ofHours(ACCESS_TOKEN_EXPIRATION));
+    }
 }
