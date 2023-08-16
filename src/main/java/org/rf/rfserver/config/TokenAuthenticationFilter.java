8bd5fb9d75bbb74d49a9dc93b35806233a170ff9
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-08-16 18:27:07 +0900

    feature: [#17] token을 적용하면 허가되지 않은 api도 사용 가능합니다

--- src/main/java/org/rf/rfserver/config/TokenAuthenticationFilter.java
+++ src/main/java/org/rf/rfserver/config/TokenAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.rf.rfserver.config.jwt.TokenProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
--- src/main/java/org/rf/rfserver/config/WebSecurityConfig.java
+++ src/main/java/org/rf/rfserver/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package org.rf.rfserver.config;
 
 import lombok.RequiredArgsConstructor;
+import org.rf.rfserver.config.jwt.TokenProvider;
 import org.rf.rfserver.user.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -20,17 +23,23 @@ public class WebSecurityConfig {
     private final UserDetailsService userDetailsService;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public TokenAuthenticationFilter tokenAuthenticationFilter(TokenProvider tokenProvider) {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, TokenAuthenticationFilter tokenAuthenticationFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests ->
                         requests.requestMatchers("/", "/swagger-ui/**", "/user", "/party"
-                                        , "/user/login", "/enums").permitAll()	// requestMatchers의 인자로 전달된 url은 모두에게 허용
-                                .anyRequest().authenticated()	// 그 외의 모든 요청은 인증 필요
+                                        , "/user/login", "/enums").permitAll() // requestMatchers의 인자로 전달된 url은 모두에게 허용
+                                .anyRequest().authenticated() // 그 외의 모든 요청은 인증 필요
                 )
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )	// 세션을 사용하지 않으므로 STATELESS 설정
+                ) // 세션을 사용하지 않으므로 STATELESS 설정
+                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
