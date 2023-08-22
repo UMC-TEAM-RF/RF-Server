c5203dffade9985485ae7fb66ff2de6f24053f7a
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2023-08-22 16:00:46 +0900

    fix: [#107] isFullofKorean 함수 타입 수정

--- src/main/java/org/rf/rfserver/party/service/PartyService.java
+++ src/main/java/org/rf/rfserver/party/service/PartyService.java
@@ -147,6 +147,7 @@ public class PartyService {
         isFullParty(party);
         if (userService.isKorean(user)) {
             isFullOfKorean(party);
+        }
         if(userService.isKorean(user)) {
             if (isFullOfKorean(party)) {
                 throw new BaseException(FULL_OF_KOREAN);
@@ -156,6 +157,7 @@ public class PartyService {
         isJoinedUser(user, party);
     }
 
+
     public boolean isFullParty(Party party) {
         if (party.getUsers().size() >= party.getMemberCount()) {
             return true;
@@ -163,11 +165,13 @@ public class PartyService {
         return false;
     }
 
-    public void isFullOfKorean(Party party) throws BaseException {
+    public Boolean isFullOfKorean(Party party) throws BaseException {
         if(party.getNativeCount() <= party.getCurrentNativeCount()) {
-            throw new BaseException(FULL_OF_KOREAN);
+            return true;
         }
+        throw new BaseException(FULL_OF_KOREAN);
     }
+
 
     public void isJoinedUser(User user, Party party) throws BaseException {
         for (UserParty userParty : party.getUsers()) {
