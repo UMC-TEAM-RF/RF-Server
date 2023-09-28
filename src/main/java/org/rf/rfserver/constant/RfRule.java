47433f5c697c61cb9fb5bdc253ee3c17f2daeca0
오형석 <77862152+kuk6933@users.noreply.github.com>
2023-09-28 22:03:29 +0900

    feat: [#131]  Security 적용

--- src/main/java/org/rf/rfserver/constant/RfRule.java
+++ src/main/java/org/rf/rfserver/constant/RfRule.java
@@ -2,6 +2,6 @@ package org.rf.rfserver.constant;
 
 public class RfRule {
     public static final int MAX_PARTY_NUMBER = 5;
-    public static final int ACCESS_TOKEN_EXPIRATION = 2;
+    public static final int ACCESS_TOKEN_EXPIRATION = 70;
     public static final int REFRESH_TOKEN_EXPIRATION = 7;
 }
