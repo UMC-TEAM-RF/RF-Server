b4ab056a5e1396ae5276676ab6178135f72e8da4
minjgziii <kmj4668@gmail.com>
2023-11-10 20:03:32 +0900

    feature [#151] : 아이디 찾기 위한 이메일 세부 메세지 설정

--- src/main/java/org/rf/rfserver/mail/domain/MailMessage.java
+++ src/main/java/org/rf/rfserver/mail/domain/MailMessage.java
@@ -9,7 +9,10 @@ public enum MailMessage {
     MAIL_TITLE("[알프] 인증번호를 발송했습니다."),
     MAIL_MESSAGE("[학교 인증을 위한 메일]\n"
             + "이메일 인증을 진행해 주세요.\n"
-            + "아래의 인증번호를 입력하여 회원가입을 완료하세요.\n")
+            + "아래의 인증번호를 입력하여 회원가입을 완료하세요.\n"),
+    FINDID_MESSAGE("[아이디 찾기를 위한 메일]\n"
+            + "이메일 인증을 진행해 주세요.\n"
+            + "아래의 인증번호를 입력해 주세요.\n")
     ;
 
     private final String content;
